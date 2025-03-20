44d50d19f76ed61247b5a52262e6e8fdce84b329
dhiwakargk <dhiwakargk@gmail.com>
2025-03-20 10:18:41 +0530

    backend

---
+++ backend/database.js
@@ -0,0 +1,45 @@
+const mongodb=require("mongoose")
+mongodb.connect("mongodb://localhost:27017").then(()=>{
+    console.log("Data base connected successfully")
+}).catch(()=>{
+    console.log("Data Base not connected")
+})
+
+const table=new mongodb.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    ph:{
+        type:Number,
+        required:true
+    },
+    feed_back_type:{
+        type:String,
+        required:true
+    },
+    feed_back_description:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+        required:true
+    }
+})
+
+const feedback=mongodb.model("Feedbacks",table)
+
+const Create_Feed_back=async(name,email,ph,feed_back_type,feed_back_description,rating)=>{
+    const add_feedback=new feedback({name:name,email:email,ph:ph,feed_back_type:feed_back_type ,feed_back_description:feed_back_description,rating:rating})
+    await add_feedback.save()
+}
+
+
+
+module.exports={Create_Feed_back}
+
---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+app.use(cors())
+const mongodb=require("mongoose")
+const DB=require("./database")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+
+app.post("/feedback",async(req,res)=>{
+    console.log(req.body)
+    const add_feedback=DB.Create_Feed_back(req.body.name,req.body.email,req.body.phoneNumber,req.body.feedbackType,req.body.feedbackDescription,req.body.rating)
+    
+    res.json({"result":"Data Added Successfuly"})
+
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server Started")
+})
